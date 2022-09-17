9b44d02122183b0224a24b3e4a9bf898066710b9
henriquelucasdf <henriquelucasdf@gmail.com>
2022-09-17 13:56:10 -0300

    fix: Fixed test bug

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	total := Sum(15, 15)
+	total := sum(15, 15)
 
 	if total != 30 {
 		t.Errorf("Result %d is invalid - Expected: %d", total, 30)
